8423e21137f036a5d858e453931283c75eb002cd
0ksorn <oguzaktas04@hotmail.com>
2026-04-11 09:57:33 +0200

    literally nothing. Just a bit of tidying up

---
+++ lua/plugins/mini_surround.lua
@@ -0,0 +1,19 @@
+return {
+  "nvim-mini/mini.surround",
+  keys = function(_, keys)
+    -- Populate the keys based on the user's options
+    local opts = LazyVim.opts("mini.surround")
+    local mappings = {
+      { opts.mappings.add, desc = "Add Surrounding", mode = { "n", "x" } },
+    }
+    mappings = vim.tbl_filter(function(m)
+      return m[1] and #m[1] > 0
+    end, mappings)
+    return vim.list_extend(mappings, keys)
+  end,
+  opts = {
+    mappings = {
+      add = "gsa", -- Add surrounding in Normal and Visual modes
+    },
+  },
+}
