66bbf4d096d6b8f12ed4e421f48306d0678d030e
0ksorn <oguzaktas04@hotmail.com>
2026-04-28 23:52:58 +0200

    snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -69,6 +69,13 @@ return {
       },
       sections = {
         { section = "header" },
+        {
+          pane = 2,
+          section = "terminal",
+          cmd = "cmatrix -b -C green -u 10 -s",
+          height = 10,
+          padding = 1,
+        },
         { section = "keys", gap = 1, padding = 1 },
         { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
         { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
