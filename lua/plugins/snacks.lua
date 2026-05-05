cab866993695450242b58034e2d36ae6c711a8e0
0ksorn <oguzaktas04@hotmail.com>
2026-05-05 13:40:37 +0200

    non recursive search on dashboard added

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -20,11 +20,17 @@ return {
         ---@type snacks.dashboard.Item[]
         keys = {
           { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          {
+            icon = " ",
+            key = "F",
+            desc = "Find File non-recursive",
+            action = ":lua require('fzf-lua').files({ cmd = 'find . -maxdepth 1 -type f' })",
+          },
           { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
           { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
           { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
           {
-            icon = " ",
+            icon = "",
             key = "c",
             desc = "Config",
             action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
