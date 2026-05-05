d7f6d9a6d6bf5785faee49b47bed40decd056642
0ksorn <oguzaktas04@hotmail.com>
2026-05-05 13:13:47 +0200

    stuff

--- lua/plugins/tokyo_night.lua
+++ lua/plugins/tokyo_night.lua
@@ -1,10 +1,10 @@
 return {
   "folke/tokyonight.nvim",
   opts = {
-    transparent = true,
+    -- transparent = true,
     styles = {
-      sidebars = "transparent",
-      floats = "transparent",
+      -- sidebars = "transparent",
+      -- floats = "transparent",
     },
   },
 }
